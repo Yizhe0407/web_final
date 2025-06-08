fc68dfe8003e9338879be52d185c769f7d6fa296
yizhe <liaoyizhe75@gmail.com>
2025-06-08 15:55:03 +0800

    🔧 feat: Update API destination URL to point to the production backend

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:4000/api/:path*"
+        destination: "https://dorm-reservation-backend-585e0b5f4648.herokuapp.com/api/:path*"
       },
     ]
   },
